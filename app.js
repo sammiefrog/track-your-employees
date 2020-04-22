e13fce75180a4f05f0269531f23c02f4fcf72306
sammiefrog <sammiefrog@live.com>
2020-04-22 14:20:50 -0500

    adding departments and roles

--- app.js
+++ app.js
@@ -1,70 +1,75 @@
-const inquirer = require('inquirer');
+const ask = require('inquirer');
 const validator = require('validator');
 const cfonts = require('cfonts');
 const path = require("path");
 const fs = require("fs");
+// consttable = require('console.table');
 const connection = require('./connect');
 
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     // for (var i = 0; i < res.length; i++) {
-//     //     console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
-//     // }
-//     // console.table(res);
-//     console.log("connected as id " + connection.threadId);
-//     inquireQ();
-// });
+connection.connect(function (err) {
+    if (err) throw err;
+    // for (var i = 0; i < res.length; i++) {
+    //     console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+    // }
+    // console.table(res);
+    console.log("connected as id " + connection.threadId);
+    inquireQ();
+});
 
-// const questions = [{
-//     type: "input",
-//     message: "Please enter employee's full name:",
-//     name: "fullName",
-//     validate: value => {
-//         var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-//         if (!regName.test(value)) {
-//             return "'Please enter your first & last name";
-//         }
-//         return true;
-//     }
-// },
-// {
-//     type: "input",
-//     message: "Please employee's id numer:",
-//     name: "id",
-//     validate: value => {
-//         if (validator.isInt(value)) {
-//             return true;
-//         }
-//         return "Please enter a valid ID Number.";
-//     }
-// },
-// {
-//     type: "input",
-//     message: "Please enter employee's email:",
-//     name: "email",
-//     validate: value => {
-//         if (validator.isEmail(value)) {
-//             return true;
-//         }
-//         return "Please enter a valid e-mail address.";
-//     }
-// },
-// {
-//     type: "list",
-//     message: "Select employee's role:",
-//     choices: ['Manager', 'Engineer', 'Intern'],
-//     name: "role"
-// }
-// ];
+const addDept = [{
+    type: "input",
+    message: "Please enter the department you wish to add:",
+    name: "department",
+    validate: value => {
+        if (validator.isAlpha(value)) {
+            return true;
+        }
+        return "Please enter a valid department (a-z)";
+    }
+}];
+
+const addRole = [{
+    type: "input",
+    message: "Please enter the role you wish to add:",
+    name: "title",
+    validate: value => {
+        if (validator.isAlpha(value)) {
+            return true;
+        }
+        return "Please enter valid role (a-z)";
+    }
+},
+{
+    type: "input",
+    massage: "Please enter the salary for this role:",
+    name: "salary",
+    validate: value => {
+        if (validator.isInt(value)) {
+            return true;
+        }
+        return "Please enter a valid salary ex:(3000.00)";
+    }
+},
+{
+    type: "input",
+    massage: "Please enter the department ID for this role:",
+    name: "department_id",
+    validate: value => {
+        if (validator.isInt(value)) {
+            return true;
+        }
+        return "Please enter a valid department ID (number)";
+    }
+}];
 
 const inquireQ = () => {
-    inquirer
+    ask
         .prompt([
             // build or finish sets up switch case
             {
                 type: "list",
                 message: "What would you like to do?",
-                choices: ["Add Department", "View Department", "Add Roles", "View Roles", "Add Employees", "View Employees", "Update Employee Roles", "Update Employee Managers", "View Employees by Manager", "Delete Departments", "Delete Roles", "Delete Employees"],
+                choices: ["Add Department", "View Department", "Add Roles", "View Roles", "Add Employees", "View Employees", "Update Employee Roles", "Update Employee Managers", "View Employees by Manager", "Delete Departments", "Delete Roles", "Delete Employees", "Finish"],
                 name: "userFunction"
             }
         ]).then(res => {
@@ -72,16 +77,60 @@ const inquireQ = () => {
             //switch case for all options
             switch (userFunction) {
                 case "Add Department":
+                    ask.prompt(addDept).then(answer => {
+                        connection.query("INSERT INTO departments SET ?",
+                            {
+                                name: answer.department,
+                            },
+                            function (err) {
+                                if (err) throw err;
+                                console.log("Successfully added department!");
+                                //show the departments
+                                connection.query("SELECT * FROM departments", function (err, res) {
+                                    if (err) throw err;
+                                    res.length > 0 && console.table(res);
+                                    inquireQ();
+                                });
+                                inquireQ();
+                            });
+                    })
+
                     break;
 
-                case "View Department":
+                case "View Departments":
+                    connection.query("SELECT * FROM departments", function (err, res) {
+                        if (err) throw err;
+                        console.log(res);
+                        res.length > 0 && console.table(res);
+                        inquireQ();
+                    });
                     break;
 
                 case "Add Roles":
+                    ask.prompt(addRole).then(answer => {
+                        connection.query("INSERT INTO roles SET ?",
+                            {
+                                title: answer.title,
+                                salary: answer.salary,
+                                department_id: answer.department_id
+                            },
+                            function (err) {
+                                if (err) throw err;
+                                console.log("Successfully added role!");
+                                //view the roles
+                                connection.query("SELECT * FROM roles", function (err, res) {
+                                    if (err) throw err;
+                                    console.log(res);
+                                    res.length > 0 && console.table(res);
+                                    inquireQ();
+                                });
+                                inquireQ();
+                            });
+                    })
                     break;
 
                 case "View Roles":
-                    connection.query("SELECT * FROM role", function (err, res) {
+                    connection.query("SELECT * FROM roles", function (err, res) {
                         if (err) throw err;
                         console.log(res);
                         res.length > 0 && console.table(res);
@@ -93,6 +142,12 @@ const inquireQ = () => {
                     break;
 
                 case "View Employees":
+                    connection.query("SELECT * FROM employees", function (err, res) {
+                        if (err) throw err;
+                        console.log(res);
+                        res.length > 0 && console.table(res);
+                        inquireQ();
+                    });
                     break;
 
                 case "Update Employee Roles":
@@ -113,6 +168,9 @@ const inquireQ = () => {
                 case "Delete Employees":
                     break;
 
+                case "Finish":
+                    break;
+
                 default:
                     break;
                 //end of switch
@@ -120,7 +178,6 @@ const inquireQ = () => {
         });
 }
 
-inquireQ();
 
 
 
--- seeds.sql
+++ seeds.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT(30) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT(30) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(45) NOT NULL,
   salary  DECIMAL NOT NULL,
-  department_id INT(30) NOT NULL,
+  department_id INT(30) NOT NULL
 );
 
-CREATE TABLE  employee (
+CREATE TABLE  employees (
   id INT(30) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -25,14 +25,14 @@ CREATE TABLE  employee (
 );
 
 
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES ("Finance");
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 30000.00, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Janet", "Smith", 1, null);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Bob", "Barker", 1, 1);
